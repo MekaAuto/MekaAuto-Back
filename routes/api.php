f4e6843897e632bc95c6611e5237489fd0b69570
abrahan pulido <thehackeroffire@gmail.com>
2023-08-25 14:12:05 -0400

    v-3

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
+
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
+//GOOGLE
+Route::post('/auth/googleUser', [AuthController::class, 'googleUser']);
 
-Route::middleware('auth:sanctum')->get('/note', [NoteController::class, 'index']);
-Route::middleware('auth:sanctum')->post('/note', [NoteController::class, 'store']);
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/users', [UserController::class, 'getAuthenticatedUser'])->name('users.getAuthenticatedUser'); //good
+    Route::post('/logout', [AuthController::class, 'logoutUser']);
+
+    //Articles
+    Route::get('/note', [NoteController::class, 'index'])->name('posts.index');
+    Route::get('/note/show', [NoteController::class, 'show'])->name('posts.show');
+    Route::post('/note/store', [NoteController::class, 'store'])->name('posts.store'); //good
+    Route::put('note/id', [NoteController::class, 'update'])->name('posts.update'); //good
+    Route::delete('note/id', [NoteController::class, 'destroy'])->name('posts.delete');
+
+});
+
